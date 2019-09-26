500dc90f3ff68d92aaba4a6e9059800399e7e527
prasobchoke.ta <prasobchoke.ta@itdev7-MacBook-Pro.local>
2019-09-26 17:17:23 +0700

    added lodash , react-native-svg

--- react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    dependencies: {
-      '<dependency>': {
-        platforms: {
-          android: null, // disable Android platform, other platforms will still autolink
-        },
-      },
-    },
-  };
