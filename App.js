0fe9354bc9e717dbe51fe4ce9a750e443bef1804
virunkumarn.si <virunkumarn.si>
2019-09-26 12:48:25 +0700

    Test

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Button, Text } from 'native-base';
-export default class ButtonOutlineExample extends Component {
+import { Container, Header, Content, Button,Text } from 'native-base';
+export default class RefExample extends Component {
   render() {
     return (
       <Container>
         <Header />
         <Content>
-          <Button bordered light>
-            <Text>Light</Text>
-          </Button>
-          <Button bordered>
-            <Text>Primary</Text>
-          </Button>
-          <Button bordered success>
-            <Text>Success</Text>
-          </Button>
-          <Button bordered info>
-            <Text>Info</Text>
-          </Button>
-          <Button bordered warning>
-            <Text>Warning</Text>
-          </Button>
-          <Button bordered danger>
-            <Text>Danger</Text>
-          </Button>
-          <Button bordered dark>
-            <Text>Dark</Text>
+          <Button ref={ (c) => this._button = c }>
+           <Text>
+             P'Plew
+             </Text>
           </Button>
         </Content>
       </Container>
